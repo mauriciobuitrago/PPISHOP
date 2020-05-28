f59a8703ea43121c2de527c3873d4f6ac73c8948
Deisy <mauriciobuitrago163028@gmail.com>
2020-05-27 22:33:54 -0500

    inicio de modales

--- shop.Web/Startup.cs
+++ shop.Web/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens;       
 using shop.Web.Data;
 using shop.Web.Data.Entities;
 using shop.Web.Data.Repositories;
